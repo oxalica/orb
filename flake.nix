49ae589bd13282df0acec032f31d40cfdfca7c51
oxalica <oxalicc@pm.me>
2024-03-21 09:27:23 -0400

    Package with Nix flakes

---
+++ flake.nix
@@ -0,0 +1,52 @@
+rec {
+  description = "OneDrive Block Device Daemon";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    packages = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      rev = self.rev or (lib.warn "Git changes are not committed" (self.dirtyRev or "dirty"));
+    in rec {
+      default = orb;
+      orb = with pkgs; rustPlatform.buildRustPackage {
+        pname = "orb";
+        version = "git-${rev}";
+        src = self;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          # WAIT: onedrive-api release.
+          allowBuiltinFetchGit = true;
+        };
+
+        nativeBuildInputs = [ pkg-config rustPlatform.bindgenHook ];
+        buildInputs = [ linuxHeaders openssl ];
+
+        meta = {
+          inherit description;
+          homepage = "https://github.com/oxalica/orb";
+          mainProgram = "orb";
+        };
+      };
+    });
+
+    devShells = eachSystem (system: {
+      without-rust =
+        with nixpkgs.legacyPackages.${system};
+        mkShell {
+          nativeBuildInputs = [ pkg-config rustPlatform.bindgenHook ];
+          buildInputs = [ linuxHeaders openssl ];
+          env = {
+            RUST_BACKTRACE = "1";
+            RUST_LOG = "debug,orb=trace";
+          };
+        };
+    });
+  };
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> { };
-mkShell {
-  nativeBuildInputs = [ pkg-config rustPlatform.bindgenHook moreutils ];
-  buildInputs = [ linuxHeaders openssl ];
-  env = {
-    RUST_BACKTRACE = "1";
-    RUST_LOG = "debug,orb=trace";
-  };
-}
