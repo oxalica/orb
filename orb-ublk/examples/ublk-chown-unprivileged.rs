b4d9ac9f4d5423a9419ce407dcc2c4ee4a28688d
oxalica <oxalicc@pm.me>
2024-04-01 22:47:02 -0400

    Treat EBUSY as success in ublk-chown-unprivileged

--- orb-ublk/examples/ublk-chown-unprivileged.rs
+++ orb-ublk/examples/ublk-chown-unprivileged.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 use orb_ublk::{ControlDevice, FeatureFlags, BDEV_PREFIX, CDEV_PREFIX};
+use rustix::io::Errno;
 
 fn main() -> io::Result<()> {
     let path = std::env::args()
@@ -13,7 +14,13 @@ fn main() -> io::Result<()> {
         .ok_or_else(|| io::Error::other("argument should be /dev/ublk{b,c}NUM"))?;
 
     // Open the file to prevent racing deletion.
-    let f = std::fs::File::open(&path)?;
+    let f = match std::fs::File::open(&path) {
+        Ok(f) => f,
+        // `EBUSY` is reported on the control device when is is opened, likely by the program
+        // itself with root permission. In this case, we do not need to participate.
+        Err(err) if err.raw_os_error() == Some(Errno::BUSY.raw_os_error()) => return Ok(()),
+        Err(err) => return Err(err),
+    };
     let ctl = ControlDevice::open()?;
     let info = ctl.get_device_info(id)?;
     assert_eq!(info.dev_id(), id);
